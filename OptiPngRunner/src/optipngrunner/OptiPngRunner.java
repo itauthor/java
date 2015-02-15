b3483b3d882763c27fbc24a093f0a686e1ed6553
itauthor <alistair@itauthor.com>
2015-02-15 13:42:12 +0000

    Whitespace change: testing git in Eclipse

--- OptiPngRunner/src/optipngrunner/OptiPngRunner.java
+++ OptiPngRunner/src/optipngrunner/OptiPngRunner.java
@@ -261,13 +261,6 @@ public class OptiPngRunner {
 
 	
 	
-
-	
-
-	
-	
-	
-	
 	//TODO: CURRENTLY I JUST DUMPED THIS IN HERE ... NEED TO DO SOMETHING WITH IT ...............
     private void runExternalProg() throws IOException {
     	String prog = "optipng.exe";
