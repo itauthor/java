5cf4fc5cfd3e9e009805a023964b886b6a8fb1cc
itauthor <alistair@itauthor.com>
2015-02-20 21:36:02 +0000

    2-java-file program. Nice and neat. Happy with this.

--- test-java-stuff/src/FileList.java
+++ test-java-stuff/src/FileList.java
@@ -1,30 +1,31 @@
 import java.io.File;
 import java.util.ArrayList;
 
-
 public class FileList {
 
 	// Instantiate the ArrayList that will contain the list of files:
 	static ArrayList<String> contents = new ArrayList<>();
 
-	
-	public static void getFilePaths(File path, String ext) {
-        File[] filesInDir = path.listFiles();
-        if (filesInDir == null) return;
-        
-        String file;
+	public static int getFilePaths(String path, String ext) {
+		
+		File theDir = new File(path);
+		
+		if (!theDir.exists()) {
+			return 0;
+		}
 
-        for (File f: filesInDir) {
-            if (f.isDirectory()) {
-            	getFilePaths(f, ext);
-            }
-            else {
-				file = f.getAbsolutePath();
+		File[] filesInDir = theDir.listFiles();
+		for (File f : filesInDir) {
+			String file = f.getAbsolutePath();
+			if (f.isDirectory()) {
+				getFilePaths(file, ext);
+			} else {
 				if (file.toLowerCase().endsWith("." + ext.toLowerCase())) {
 					contents.add(file);
-				}            	
-            }
-        }
-    }	
+				}
+			}
+		}
+		return 1;
+	}
 
 }
--- test-java-stuff/src/RecursiveFilteredDirContentsLister.java
+++ test-java-stuff/src/RecursiveFilteredDirContentsLister.java
@@ -1,31 +1,22 @@
-import java.io.File;
-
 public class RecursiveFilteredDirContentsLister {
 	
 	public static void main(String[] args) {
 		// Directory to parse, extension to filter on, and max in output list:
 		String path = "C:\\Alistair\\family\\";
-		String ext = "pdf";
+		String ext = "txt";
 		int maxToShow = 15;
 
-		// Check the directory exists, or exit:
-		File theDir = new File(path);
-		if (!theDir.exists()) {
-			System.out.println("No such directory: " + path);
-			System.exit(0);
-		}
-
-		// Build the list:
-		FileList.getFilePaths(theDir, ext);		
-
-		// Only list the files if there are fewer than 10:
-		int numFiles = FileList.contents.size();
-		System.out.println("Number of files found: " + numFiles);
-		if (numFiles <= maxToShow) {
-			for(String f: FileList.contents) {
-				System.out.println(f);
-			}
+		int retVal = FileList.getFilePaths(path, ext);	
+		if (retVal == 0) {
+			System.out.println("Directory not found: " + path);
+		} else {
+			int numFiles = FileList.contents.size();
+			System.out.println("Number of files found: " + numFiles);
+			if (numFiles <= maxToShow) {
+				for(String f: FileList.contents) {
+					System.out.println(f);
+				}
+			}			
 		}
 	}
-
 }
