2424f9f16f5f24366c6909195d92473f972752fa
itauthor <alistair@itauthor.com>
2015-02-21 13:00:07 +0000

    initial files

---
+++ com.itauthor.easyOptiPNG/src/com/itauthor/easyOptiPNG/EasyOptiPNG.java
@@ -0,0 +1,37 @@
+package com.itauthor.easyOptiPNG;
+
+public class EasyOptiPNG {
+
+	public static void main(String[] args) {
+		//Display the GUI
+		
+		
+		
+		//Get the input - i.e. path & ext
+		
+		
+		
+		//Send the path & ext to getFilePaths() in FileList.java
+		
+		
+		
+		//If all is well, and files were found,
+		//Check number of files
+		//If the original path selected was a directory rather than a file:
+		// - if more than n files, tell user, warn it will take time, ask to confirm
+		// - show a dialog box listing the file(s) to be converted, ask user to confirm
+		
+		
+		
+		//If user confirmed 
+		//Make a string out of the files paths, to pass to OptiPNG
+
+		
+		
+		//run OptiPNG against the file paths
+
+		
+		
+	}
+
+}
---
+++ com.itauthor.easyOptiPNG/src/com/itauthor/easyOptiPNG/FileList.java
@@ -0,0 +1,33 @@
+package com.itauthor.easyOptiPNG;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class FileList {
+
+	// Instantiate the ArrayList that will contain the list of files:
+	static ArrayList<String> contents = new ArrayList<>();
+
+	public static int getFilePaths(String path, String ext) {
+		
+		File theDir = new File(path);
+		
+		if (!theDir.exists()) {
+			return 0;
+		}
+
+		File[] filesInDir = theDir.listFiles();
+		for (File f : filesInDir) {
+			String file = f.getAbsolutePath();
+			if (f.isDirectory()) {
+				getFilePaths(file, ext);
+			} else {
+				if (file.toLowerCase().endsWith("." + ext.toLowerCase())) {
+					contents.add(file);
+				}
+			}
+		}
+		return 1;
+	}
+
+}
